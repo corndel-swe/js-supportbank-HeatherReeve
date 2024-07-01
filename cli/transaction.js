ac4755478ce9f1345de20d710df7130b6dfd5238
heather Reeve <heather.reeve@scope.org.uk>
2024-07-01 12:20:00 +0100

    added

---
+++ cli/transaction.js
@@ -0,0 +1,12 @@
+import { Command } from 'commander'
+
+const transactionController = new Command('transaction')
+
+transactionController
+  .command('log <from> <to> <amount>')
+  .description('Log transaction data to the console')
+  .action((from, to, amount) => {
+    console.log("hello")
+  })
+
+export default transactionController
